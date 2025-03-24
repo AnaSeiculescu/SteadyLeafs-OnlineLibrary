d1ea68b4d7294ba3924f4b61532f8bed91e35206
Ana Seiculescu <anaseiculescu@gmail.com>
2025-03-24 14:33:51 +0200

    Initial commit

---
+++ src/main/java/com/onlinelibrary/steadyleafs/SteadyleafsApplication.java
@@ -0,0 +1,13 @@
+package com.onlinelibrary.steadyleafs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteadyleafsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteadyleafsApplication.class, args);
+	}
+
+}
