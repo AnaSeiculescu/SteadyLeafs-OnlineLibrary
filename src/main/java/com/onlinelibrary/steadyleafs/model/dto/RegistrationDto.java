97c5f8d30c089c35a1080f41ec5d4a5bcb468fdc
Ana Seiculescu <anaseiculescu@gmail.com>
2025-04-29 12:42:30 +0300

    repair register functionality, adding error validation messages

--- src/main/java/com/onlinelibrary/steadyleafs/model/dto/RegistrationDto.java
+++ src/main/java/com/onlinelibrary/steadyleafs/model/dto/RegistrationDto.java
@@ -17,14 +17,18 @@ import java.util.ArrayList;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegistrationDto {
-	@NotBlank
-	@Email
+
+	@NotBlank(message = "Email is required")
+	@Email(message = "Invalid email format")
 	private String email;
-	@NotBlank
+
+	@NotBlank(message = "Password is required")
 	private String password;
-	@NotBlank
+
+	@NotBlank(message = "First name is required")
 	private String firstName;
-	@NotBlank
+
+	@NotBlank(message = "Last name is required")
 	private String lastName;
 
 	private User user = new User();
