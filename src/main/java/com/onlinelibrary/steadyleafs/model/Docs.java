6f484952c0dd45022e3fe7ee9aa505e185fed729
Ana Seiculescu <anaseiculescu@gmail.com>
2025-05-05 16:25:07 +0300

    start integrating Open Library API for fetching book covers

---
+++ src/main/java/com/onlinelibrary/steadyleafs/model/Docs.java
@@ -0,0 +1,14 @@
+package com.onlinelibrary.steadyleafs.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Docs {
+	private int cover_i;
+}
---
+++ src/main/java/com/onlinelibrary/steadyleafs/service/BookCoverApiService.java
@@ -0,0 +1,35 @@
+package com.onlinelibrary.steadyleafs.service;
+
+import com.onlinelibrary.steadyleafs.model.OpenLibraryCovers;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@Slf4j
+@AllArgsConstructor
+public class BookCoverApiService {
+	private static final String OPEN_LIBRARY_API_URL = "https://openlibrary.org/search.json?title=";
+
+	RestTemplate restTemplate = new RestTemplate();
+
+//	private final String bookTitle;
+
+	public String getCoverUrl(String bookTitle) {
+		ResponseEntity<OpenLibraryCovers[]> response = restTemplate.getForEntity(OPEN_LIBRARY_API_URL + bookTitle, OpenLibraryCovers[].class);
+
+		if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
+			OpenLibraryCovers[] covers = response.getBody();
+
+			if (covers.length > 0 && covers[0] != null && covers[0].getDocs() != null) {
+				int coverId = covers[0].getDocs().getCover_i();
+				return "https://covers.openlibrary.org/b/id/" + coverId + "-M.jpg";
+			}
+		}
+
+		return null;
+	}
+
+}
