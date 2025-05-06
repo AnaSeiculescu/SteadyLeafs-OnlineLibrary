bb3d43d7e9d10776d26d79d78a6245c344d0d50a
Ana Seiculescu <anaseiculescu@gmail.com>
2025-05-06 10:04:33 +0300

    style the books presentation

--- src/main/java/com/onlinelibrary/steadyleafs/service/BookCoverApiService.java
+++ src/main/java/com/onlinelibrary/steadyleafs/service/BookCoverApiService.java
@@ -1,7 +1,5 @@
 package com.onlinelibrary.steadyleafs.service;
 
-//import com.onlinelibrary.steadyleafs.model.OpenLibraryCovers;
-//import lombok.AllArgsConstructor;
 import com.onlinelibrary.steadyleafs.model.Doc;
 import com.onlinelibrary.steadyleafs.model.OpenLibraryCovers;
 import lombok.extern.slf4j.Slf4j;
@@ -11,13 +9,10 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 @Slf4j
-//@AllArgsConstructor
 public class BookCoverApiService {
 	private static final String OPEN_LIBRARY_API_URL = "https://openlibrary.org/search.json?title=";
 
 	RestTemplate restTemplate = new RestTemplate();
-
-//	private final String bookTitle;
 
 	public String getCoverUrl(String bookTitle) {
 		ResponseEntity<OpenLibraryCovers> response = restTemplate.getForEntity(OPEN_LIBRARY_API_URL + bookTitle, OpenLibraryCovers.class);
