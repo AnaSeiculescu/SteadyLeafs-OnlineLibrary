2b3a9dff80820e010453cd9c60874cd7534f9b9e
Ana Seiculescu <anaseiculescu@gmail.com>
2025-05-26 16:41:13 +0300

    add password toggle for the register form

---
+++ src/main/resources/static/js/passwordToggle.js
@@ -0,0 +1,13 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const togglePassword = document.querySelector('#togglePassword');
+    const passwordInput = document.querySelector('#password');
+
+    togglePassword.addEventListener('click', () => {
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordInput.setAttribute('type', type);
+
+        togglePassword.querySelector('i').classList.toggle('bi-eye');
+        togglePassword.querySelector('i').classList.toggle('bi-eye-slash');
+    });
+});
